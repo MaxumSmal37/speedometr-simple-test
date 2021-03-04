a584e63695ca206014aaa40b3230c620e30a9858
MaxumSmal37 <40183260+MaxumSmal37@users.noreply.github.com>
2021-03-05 00:24:45 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function fun1() {
+  var rng=document.getElementById('r1'); //rng - это Input
+  var p=document.getElementById('one'); // p - абзац
+  p.innerHTML=rng.value;
+  
+}
+const Rov = document.querySelector(.)
+var a
+var b = 30
+
+    a = (180 * b)/100
+
+console.log(a)
+
+
+
